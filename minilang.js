e0f707b61adc2223f0479948bf471cdfce96ee0c
Kyle McCurley <kmccurley123@gmail.com>
2020-08-08 01:03:46 -0400

    Create minilang.js

---
+++ minilang.js
@@ -0,0 +1,69 @@
+function minilang(program) {
+  let sections = program.split(' ');
+  let stack = [];
+  let register = 0;
+
+  for (let i = 0; i < sections.length; i++) {
+    let el = sections[i];
+    switch (el) {
+      case 'PUSH':
+        stack.push(register);
+        break;
+      case 'ADD':
+        register += stack.pop();
+        break;
+      case 'SUB':
+        register -= stack.pop();
+        break;
+      case 'MULT':
+        register = Math.floor(register * stack.pop())
+        break;
+      case 'DIV':
+        register = Math.floor(register / stack.pop());
+        break;
+      case 'MOD':
+        register %= stack.pop();
+        break;
+      case 'POP':
+        register = stack.pop();
+        break;
+      case 'PRINT':
+        console.log(register);
+        break;
+      default:
+        register  = Number(el);
+    }
+  }
+}
+
+minilang('PRINT');
+// 0
+
+minilang('5 PUSH 3 MULT PRINT');
+// 15
+
+minilang('5 PRINT PUSH 3 PRINT ADD PRINT');
+// 5
+// 3
+// 8
+
+minilang('5 PUSH POP PRINT');
+// 5
+
+minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT');
+// 5
+// 10
+// 4
+// 7
+
+minilang('3 PUSH PUSH 7 DIV MULT PRINT');
+// 6
+
+minilang('4 PUSH PUSH 7 MOD MULT PRINT');
+// 12
+
+minilang('-3 PUSH 5 SUB PRINT');
+// 8
+
+minilang('6 PUSH');
+// (nothing is printed because the `program` argument has no `PRINT` commands)
